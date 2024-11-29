4bf07f3bd04f49e396b712d2e38185cae177e4d3
Sumaiya khan <t3khsu00@students.oamk.fi>
2024-11-29 23:16:50 +0200

    exercise5done

---
+++ src/RecipeTag.tsx
@@ -0,0 +1,17 @@
+// File: src/RecipeTag.tsx
+import React from 'react';
+
+interface IRecipeTagProps {
+  tagName: string;
+  onSelectTag: (tagName: string) => void;
+}
+
+const RecipeTag: React.FC<IRecipeTagProps> = ({ tagName, onSelectTag }) => {
+  return (
+    <li>
+      <button onClick={() => onSelectTag(tagName)}>{tagName}</button>
+    </li>
+  );
+};
+
+export default RecipeTag;
